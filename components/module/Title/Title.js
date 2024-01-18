7f271d7a458f65a983f25653b6e823dc49a29458
mohammadmehdi <mohammad333mehdi@gmail.com>
2024-01-18 16:55:35 +0330

    fix bug html

--- components/module/Title/Title.js
+++ components/module/Title/Title.js
@@ -5,7 +5,7 @@ function Title({title , txt}) {
   return (
     <section className={style.title}>
       <div className={style.title_wrapper}>
-        <h3 className={style.title_text}>{title}<span className={style.title_text_color}>{txt}</span></h3>
+        <h3 className={style.title_text}>{title}  <span className={style.title_text_color}>{txt}</span></h3>
         <p className={style.title_paraph}>
           با نرم افزارها شناخت بیشتری را برای طراحان رایانه ای علی الخصوص طراحان
           خلاقی و فرهنگ پیشرو در زبان فارسی ایجاد کرد.
