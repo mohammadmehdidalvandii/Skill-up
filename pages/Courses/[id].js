79aa79c14482a694a47fc3a072a7474cb44335b7
mohammadmehiddalvadnii <mhmdmehdidalvandii@gmail.com>
2024-01-24 15:12:38 +0330

    create page courseDetails

---
+++ pages/Courses/[id].js
@@ -0,0 +1,15 @@
+import React from 'react'
+import Head from 'next/head'
+
+function CourseDetails() {
+  return (
+    <>
+     <Head>
+        <title>Skill-Up | جزئیات دوره</title>
+    </Head>
+    <h1>course-details</h1>
+    </>
+  )
+}
+
+export default CourseDetails
