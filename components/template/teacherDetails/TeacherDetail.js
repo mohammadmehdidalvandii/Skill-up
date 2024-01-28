725329c406172105dca297c0c5663e8f0b97154d
mohammadmehiddalvadnii <mhmdmehdidalvandii@gmail.com>
2024-01-28 14:57:48 +0330

    create section TeacherDetail and teacherDetails

---
+++ components/template/teacherDetails/TeacherDetail.js
@@ -0,0 +1,35 @@
+import React from 'react';
+import style from '@/styles/teacherDetails/teacherDetails.module.css'
+
+function TeacherDetail() {
+  return (
+   <section className={style.teacherDetail}>
+    <div className="container">
+        <div className="row">
+            <div className="col-12">
+                <div className={style.teacherDetail_content}>
+                    <img src="/images/user-1.jpg" alt="" className={style.teacherDetail_img} />
+                    <h6 className={style.teacherDetail_name}>نادیا ناصری</h6>
+                    <p className={style.teacherDetail_desc}>ارائه راهکارها و شرایط سخت تایپ به پایان رسد وزمان مورد نیاز شامل حروفچینی دستاوردهای اصلی و جوابگوی سوالات پیوسته اهل دنیای موجود طراحی اساسا مورد .</p>
+                    <ul className={style.teacherDetail_items}>
+                        <li className={style.teacherDetail_item}>
+                            <span className={style.teacherDetail_item_text}>گرافیک
+</span>
+                        </li>
+                        <li className={style.teacherDetail_item}>
+                            <span className={style.teacherDetail_item_text}>102 دوره</span>
+                        </li>
+                        <li className={style.teacherDetail_item}>
+                            <span className={style.teacherDetail_item_text}>502 شرکت کننده  </span>
+                        </li>
+                    </ul>
+
+                </div>
+            </div>
+        </div>
+    </div>
+   </section>
+  )
+}
+
+export default TeacherDetail
--- pages/Teachers/[id].js
+++ pages/Teachers/[id].js
@@ -1,16 +1,18 @@
 import React from 'react'
 import Head from 'next/head'
 import PageHeader from '@/components/module/PageHeader/PageHeader'
+import TeacherDetail from '@/components/template/teacherDetails/TeacherDetail'
 
-function teacherDetails() {
+function TeacherDetails() {
   return (
     <>
      <Head>
         <title>Skill-Up |  جزئیات - مدرس </title>
     </Head>
     <PageHeader title='جزئیات مدرس' text='جزئیات مدرس'/>
+    <TeacherDetail/>
     </>
   )
 }
 
-export default teacherDetails
+export default TeacherDetails
