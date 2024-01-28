2c6f7aa22c00e952901c15089685ba80686dcea9
mohammadmehiddalvadnii <mhmdmehdidalvandii@gmail.com>
2024-01-28 14:06:01 +0330

    create page teacherDetails

---
+++ pages/Teachers/[id].js
@@ -0,0 +1,16 @@
+import React from 'react'
+import Head from 'next/head'
+import PageHeader from '@/components/module/PageHeader/PageHeader'
+
+function teacherDetails() {
+  return (
+    <>
+     <Head>
+        <title>Skill-Up |  جزئیات - مدرس </title>
+    </Head>
+    <PageHeader title='جزئیات مدرس' text='جزئیات مدرس'/>
+    </>
+  )
+}
+
+export default teacherDetails
