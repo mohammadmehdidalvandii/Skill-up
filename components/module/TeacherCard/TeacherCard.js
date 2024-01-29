7a8c508748c52ecbc358c130054497bbfc116784
mohammadmehiddalvadnii <mhmdmehdidalvandii@gmail.com>
2024-01-29 20:24:59 +0330

    update db.json for teacher

--- components/module/TeacherCard/TeacherCard.js
+++ components/module/TeacherCard/TeacherCard.js
@@ -9,17 +9,17 @@ import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faUser } from "@fortawesome/free-solid-svg-icons";
 import Link from 'next/link';
 
-function TeacherCard() {
+function TeacherCard({image ,job, teacherName , student}) {
   return (
     <Link href='' className={style.teacherCard}>
-        <img src="/images/t-1.png" alt="teacher image" className={style.teacherCard_img} />
-        <span className={style.teacherCard_learn}>مدرس ریاضی و فیزیک</span>
-        <h6 className={style.teacherCard_teacherName}>مصطفی اسلامی</h6>
+        <img src={image} alt="teacher image" className={style.teacherCard_img} />
+        <span className={style.teacherCard_learn}>{job}</span>
+        <h6 className={style.teacherCard_teacherName}>{teacherName}</h6>
         <div className={style.teacherCard_student_iconNumber}>
             <span className={style.teacherCard_student_icon}>
                 <FontAwesomeIcon icon={faUser}/>
             </span>
-            <span className={style.teacherCard_student_number}>2,500</span>
+            <span className={style.teacherCard_student_number}>{student}</span>
         </div>
     </Link>
 //     <div className="col-lg-4 col-md-6 col-sm-12">
--- components/template/teachers/Teacher/Teacher.js
+++ components/template/teachers/Teacher/Teacher.js
@@ -1,30 +1,35 @@
-import React from 'react';
+import React, { useState } from 'react';
 import style from '@/styles/teachers/Teacher.module.css'
 import TeacherCard from '@/components/module/TeacherCard/TeacherCard';
 
+import db from '@/Data/db.json'
+
+
 function Teacher() {
+    const [Teachers ,setTeachers] = useState(db.Teacher)
+
   return (
     <section className={style.teacher}>
        <div className="container">
        <div className="row">
-            <div className="col-lg-4 col-md-4 col-sm-12 col-xs-12">
-                <TeacherCard/>
+            {
+                Teachers.map(teacher => (
+                    <div className='col-lg-4 col-md-4 col-sm-12 col-xs-12' key={teacher.id}>
+                        <TeacherCard 
+                        {...teacher}
+                        />
+                    </div>
+                ))
+            }
+
+            {/* {products.map(product => (
+            <div className="col-lg-4 col-md-4 col-sm-12 col-xs-12" key={product.id}>
+                <TeacherCard
+                    {...product}
+                    student={product.student.toLocaleString()}
+                />
             </div>
-            <div className="col-lg-4 col-md-4 col-sm-12 col-xs-12">
-                <TeacherCard/>
-            </div>
-            <div className="col-lg-4 col-md-4 col-sm-12 col-xs-12">
-                <TeacherCard/>
-            </div>
-            <div className="col-lg-4 col-md-4 col-sm-12 col-xs-12">
-                <TeacherCard/>
-            </div>
-            <div className="col-lg-4 col-md-4 col-sm-12 col-xs-12">
-                <TeacherCard/>
-            </div>
-            <div className="col-lg-4 col-md-4 col-sm-12 col-xs-12">
-                <TeacherCard/>
-            </div>
+            ))} */}
         </div>
        </div>
     </section>
