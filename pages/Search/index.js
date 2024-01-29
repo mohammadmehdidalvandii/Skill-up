dd4976c96a4078efe7e636056d8395afe60dcef3
mohammadmehiddalvadnii <mhmdmehdidalvandii@gmail.com>
2024-01-29 15:19:29 +0330

    create page search

---
+++ pages/Search/index.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import Head from 'next/head';
+import PageHeader from '@/components/module/PageHeader/PageHeader';
+
+function Search() {
+  return (
+    <>
+        <Head>
+        <title>Skill-Up | دوره ها</title>
+       </Head>
+       <PageHeader title='نتیجه جستجو...' text='نتیجه جستجو...'/>
+    </>
+  )
+}
+
+export default Search
